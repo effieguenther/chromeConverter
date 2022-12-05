6fddcea03c703521ed57187abae44aae4272440b
effieguenther <effiegguenther@gmail.com>
2022-12-04 21:11:19 -0500

    experimenting with framework

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let paragraphs = document.getElementsByTagName('p')
-for (elt of paragraphs) {
-    elt.style['background-color'] = '#FF00FF'
+console.log('extension go?')
+
+chrome.runtime.onMessage.addListener(gotMessage)
+
+function gotMessage(message, sender, sendResponse) {
+    console.log(message.txt)
+    if (message.txt === "hello") {
+        let paragraphs = document.getElementsByTagName('p')
+        for (elt of paragraphs) {
+            elt.style['background-color'] = '#FF00FF'
+        }
+    }
 }
